842592ebc8197d33465e3012a8463ca9a337695d
PMarinPy <pablo.marin1@utp.edu.co>
2024-06-18 16:02:54 -0500

    fsdaf

--- segundaprac/data.js
+++ segundaprac/data.js
@@ -1,6 +1,132 @@
-let apple = document.querySelector("apples");
-let banana = document.querySelector("bananas")
-let carrots = document.querySelector("carrots")
-let milk = document.querySelector("milk")
-
-
+export let products = {
+    fruits: {
+        apple: {
+            name: "Apple",
+            price: 0.99,
+            expire: 2024,
+            ID: "123213"
+        },
+        banana: {
+            name: "Banana",
+            price: 0.79,
+            expire: 2024,
+            ID: "123214"
+        },
+        carrot: {
+            name: "Carrot",
+            price: 1.29,
+            expire: 2024,
+            ID: "123215"
+        },
+        milk: {
+            name: "Milk",
+            price: 2.99,
+            expire: 2024,
+            ID: "123216"
+        }
+    },
+    hygiene: {
+        soap: {
+            name: "Soap",
+            price: 1.99,
+            expire: 2025,
+            ID: "223213"
+        },
+        shampoo: {
+            name: "Shampoo",
+            price: 5.49,
+            expire: 2025,
+            ID: "223214"
+        },
+        toothpaste: {
+            name: "Toothpaste",
+            price: 3.99,
+            expire: 2025,
+            ID: "223215"
+        },
+        towel: {
+            name: "Towel",
+            price: 10.99,
+            expire: 2025,
+            ID: "223216"
+        }
+    },
+    pets: {
+        dogFood: {
+            name: "Dog Food",
+            price: 19.99,
+            expire: 2023,
+            ID: "323213"
+        },
+        catLitter: {
+            name: "Cat Litter",
+            price: 14.99,
+            expire: 2023,
+            ID: "323214"
+        },
+        birdCage: {
+            name: "Bird Cage",
+            price: 59.99,
+            expire: 2023,
+            ID: "323215"
+        },
+        fishTank: {
+            name: "Fish Tank",
+            price: 29.99,
+            expire: 2023,
+            ID: "323216"
+        }
+    },
+    clothes: {
+        tshirt: {
+            name: "T-Shirt",
+            price: 9.99,
+            expire: 2024,
+            ID: "423213"
+        },
+        jeans: {
+            name: "Jeans",
+            price: 39.99,
+            expire: 2024,
+            ID: "423214"
+        },
+        jacket: {
+            name: "Jacket",
+            price: 49.99,
+            expire: 2024,
+            ID: "423215"
+        },
+        socks: {
+            name: "Socks",
+            price: 4.99,
+            expire: 2024,
+            ID: "423216"
+        }
+    },
+    homeAppliances: {
+        refrigerator: {
+            name: "Refrigerator",
+            price: 499.99,
+            expire: 2026,
+            ID: "523213"
+        },
+        washingMachine: {
+            name: "Washing Machine",
+            price: 299.99,
+            expire: 2026,
+            ID: "523214"
+        },
+        microwave: {
+            name: "Microwave",
+            price: 99.99,
+            expire: 2026,
+            ID: "523215"
+        },
+        toaster: {
+            name: "Toaster",
+            price: 29.99,
+            expire: 2026,
+            ID: "523216"
+        }
+    }
+};
--- segundaprac/script.js
+++ segundaprac/script.js
@@ -1,16 +1,15 @@
-//import { appleJSON } from './data';
+import { products } from './data.js';
+
 let butFru = document.querySelector("#buttonFruits");
 let butHygie = document.querySelector("#buttonHygiene");
 let butPets = document.querySelector("#buttonPets");
 let butCloth = document.querySelector("#buttonClothes");
 let butHome = document.querySelector("#buttonHomeAppliances");
-
 let hygieCont = document.querySelector("#hygieneCont");
 let petsCont = document.querySelector("#petsCont");
 let clothCont = document.querySelector("#clothesCont");
 let homeCont = document.querySelector("#homeAppliancesCont");
 let fruitCont = document.querySelector(".contItems");
-
 let butts = [butCloth, butFru, butHome, butPets, butHygie];
 let conts = [clothCont, fruitCont, homeCont, petsCont, hygieCont,];
 
@@ -22,139 +21,7 @@ function relationButton(butts, conts) {
     }
 }
 relationButton(butts, conts);
-let products = {
-    fruits: {
-        apple: {
-            name: "Apple",
-            price: 0.99,
-            expire: 2024,
-            ID: "123213"
-        },
-        banana: {
-            name: "Banana",
-            price: 0.79,
-            expire: 2024,
-            ID: "123214"
-        },
-        carrot: {
-            name: "Carrot",
-            price: 1.29,
-            expire: 2024,
-            ID: "123215"
-        },
-        milk: {
-            name: "Milk",
-            price: 2.99,
-            expire: 2024,
-            ID: "123216"
-        }
-    },
-    hygiene: {
-        soap: {
-            name: "Soap",
-            price: 1.99,
-            expire: 2025,
-            ID: "223213"
-        },
-        shampoo: {
-            name: "Shampoo",
-            price: 5.49,
-            expire: 2025,
-            ID: "223214"
-        },
-        toothpaste: {
-            name: "Toothpaste",
-            price: 3.99,
-            expire: 2025,
-            ID: "223215"
-        },
-        towel: {
-            name: "Towel",
-            price: 10.99,
-            expire: 2025,
-            ID: "223216"
-        }
-    },
-    pets: {
-        dogFood: {
-            name: "Dog Food",
-            price: 19.99,
-            expire: 2023,
-            ID: "323213"
-        },
-        catLitter: {
-            name: "Cat Litter",
-            price: 14.99,
-            expire: 2023,
-            ID: "323214"
-        },
-        birdCage: {
-            name: "Bird Cage",
-            price: 59.99,
-            expire: 2023,
-            ID: "323215"
-        },
-        fishTank: {
-            name: "Fish Tank",
-            price: 29.99,
-            expire: 2023,
-            ID: "323216"
-        }
-    },
-    clothes: {
-        tshirt: {
-            name: "T-Shirt",
-            price: 9.99,
-            expire: 2024,
-            ID: "423213"
-        },
-        jeans: {
-            name: "Jeans",
-            price: 39.99,
-            expire: 2024,
-            ID: "423214"
-        },
-        jacket: {
-            name: "Jacket",
-            price: 49.99,
-            expire: 2024,
-            ID: "423215"
-        },
-        socks: {
-            name: "Socks",
-            price: 4.99,
-            expire: 2024,
-            ID: "423216"
-        }
-    },
-    homeAppliances: {
-        refrigerator: {
-            name: "Refrigerator",
-            price: 499.99,
-            expire: 2026,
-            ID: "523213"
-        },
-        washingMachine: {
-            name: "Washing Machine",
-            price: 299.99,
-            expire: 2026,
-            ID: "523214"
-        },
-        microwave: {
-            name: "Microwave",
-            price: 99.99,
-            expire: 2026,
-            ID: "523215"
-        },
-        toaster: {
-            name: "Toaster",
-            price: 29.99,
-            expire: 2026,
-            ID: "523216"
-        }
-    }
-};
-console.log(products.fruits.banana)
+
 
 class aseo{
     constructor(nombre, precio, cantidad, imagen){
