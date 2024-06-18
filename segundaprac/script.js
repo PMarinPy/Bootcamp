1fd52d07d09b940bdaa0c899ca6465bdb80eea5b
PMarinPy <pablo.marin1@utp.edu.co>
2024-06-18 11:52:42 -0500

    si

--- segundaprac/script.js
+++ segundaprac/script.js
@@ -1,6 +1,21 @@
-const fruitBut = document.querySelector('#buttonFruits')
-const fruitCont = document.getElementById('fruitsCont')
+let butFru = document.querySelector("#buttonFruits");
+let butHygie = document.querySelector("#buttonHygiene");
+let butPets = document.querySelector("#buttonPets");
+let butCloth = document.querySelector("#buttonClothes");
+let butHome = document.querySelector("#buttonHomeAppliances");
 
-fruitBut.addEventListener('click', () => {
-    fruitCont.style.display = fruitCont.style.display == 'none' ? 'block' : 'none';
-});
+let hygieCont = document.querySelector("#hygieneCont");
+let petsCont = document.querySelector("#petsCont");
+let clothCont = document.querySelector("#clothesCont");
+let homeCont = document.querySelector("#homeAppliancesCont");
+let fruitCont = document.querySelector(".contItems");
+
+
+let butts = [butCloth, butFru, butHome, butPets, butHygie];
+let conts = [clothCont, fruitCont, homeCont, petsCont, hygieCont,];
+ // Corregido a 'hygieCont'
+for (let i = 0; i < butts.length; i++) {
+    butts[i].addEventListener("click", () => {
+        conts[i].style.display = conts[i].style.display === 'none' ? 'block' : 'none';
+    });
+}
