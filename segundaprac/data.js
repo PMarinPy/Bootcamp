07202d5deb85be0a2e576e0e6adeb3d70b62d5b7
PMarinPy <pablo.marin1@utp.edu.co>
2024-06-18 15:27:57 -0500

    fasdf

---
+++ segundaprac/data.js
@@ -0,0 +1,6 @@
+let apple = document.querySelector("apples");
+let banana = document.querySelector("bananas")
+let carrots = document.querySelector("carrots")
+let milk = document.querySelector("milk")
+
+
